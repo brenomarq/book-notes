9f4682f6dead0a2e5cb468031a7dea9f902daa9d
brenomarq <brenomarques516@gmail.com>
2024-01-10 11:07:07 -0300

    Connect to database

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import { password } from "./secret.js";
 
+// Initialize express application
 const app = express();
 const port = 3000;
 
+// Connect to the database
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "book_notes",
+    password: password,
+    port: 5432,
+})
+db.connect();
+
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+async function getData() {
+    const result = await db.query(`
+    SELECT title, author, cover, score, finish_date, text_content, username
+    FROM reviews
+    JOIN users ON user_id = users.id
+    JOIN books ON book_id = books.id`)
+    return result.rows;
+}
+
+app.get("/", async (req, res) => {
+    const data = await getData();
+    console.log(data);
+    res.sendStatus(200);
+})
 
 app.listen(port, () => {
     console.log(`Server running on port 3000.`)
