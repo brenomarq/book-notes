4ccf8f84490e65edfc565cae329b8a2853ba3522
brenomarq <brenomarques516@gmail.com>
2024-01-13 20:33:53 -0300

    Finish user and book methods on the api

--- booknote.api.js
+++ booknote.api.js
@@ -23,8 +23,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Users HTTP Methods
 // This one list all of the existing users
 app.get("/users", async (req, res) => {
-    const users = await db.query(`SELECT * FROM users`);
-    res.json(users.rows);
+    try {
+        const users = await db.query(`SELECT * FROM users`);
+        res.json(users.rows);
+    } catch (err) {
+        console.log(err);
+        res.json({ error: "Could not connect properly to the database."})
+    }
 });
 
 // This creates a new user
@@ -53,7 +58,72 @@ app.delete("/users/:id", async (req, res) => {
     }
 });
 
-// books
+// Books HTTP methods
+// List all the reviewed books by a certain user
+app.get("/books", async (req, res) => {
+    try {
+        const books = await db.query(`
+        SELECT * FROM books;`);
+        res.json(books.rows);
+    } catch (err) {
+        console.log(err);
+        res.json({ error: "Couldn't find the requested data. User or book does not exist."})
+    }
+});
+
+// Creates a new record of a read book
+app.post("/books", async (req, res) => {
+    try {
+        const newBook = await db.query(`
+        INSERT INTO books (title, author, cover)
+        VALUES ($1, $2, $3) RETURNING *`,
+        [req.body.title, req.body.author, req.body.cover]);
+        res.json(newBook.rows);
+    } catch (err) {
+        console.log(err);
+        res.json({ error: "Could not create a new record on the database." });
+    }
+});
+
+// Review HTTP Methods
+// list all the book reviews from a certain user
+app.get("/reviews/:id", async (req, res) => {
+    const userId = parseInt(req.params.id);
+
+    try {
+        const reviews = await db.query(`
+        SELECT title, author, cover, score, finish_date, text_content, book_id
+        FROM reviews
+        JOIN users ON users.id=user_id
+        JOIN books ON books.id=book_id
+        WHERE user_id=$1`, [userId]);
+        res.json(reviews.rows);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Could not complete the action." });
+    }
+});
+
+// Creates a new review for a book
+app.post("/reviews", async (req, res) => {
+    const userId = parseInt(req.body.user_id);
+    const bookId = parseInt(req.body.book_id);
+
+    try {
+        const newReview = await db.query(`
+        INSERT INTO reviews (score, finish_date, text_content, user_id, book_id)
+        VALUES ($1, $2, $3, $4, $5) RETURNING *`, [
+        req.body.score,
+        req.body.finish_date,
+        req.body.text_content,
+        userId,
+        bookId]);
+        res.json(newReview.rows);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ error: "Could not create the review, try again."})
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running successfully on port ${port}`);
