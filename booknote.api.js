8389cacbb6ad2d35e84afd169bef8ecca7579bbb
brenomarq <brenomarques516@gmail.com>
2024-01-27 16:26:15 -0300

    Format javascript

--- booknote.api.js
+++ booknote.api.js
@@ -9,12 +9,12 @@ const port = 4000;
 
 // Initialize connection to the database
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "book_notes", // Here you can place the name you gave to the database
-    password: password, // Here you can place your postgres password
-    port: 5432,
-})
+  user: "postgres",
+  host: "localhost",
+  database: "book_notes", // Here you can place the name you gave to the database
+  password: password, // Here you can place your postgres password
+  port: 5432,
+});
 db.connect();
 
 // middlewares
@@ -24,141 +24,158 @@ app.use(express.json());
 // Users HTTP Methods
 // This one list all of the existing users
 app.get("/users", async (req, res) => {
-    try {
-        const users = await db.query(`SELECT * FROM users`);
-        res.json(users.rows);
-    } catch (err) {
-        console.log(err);
-        res.json({ error: "Could not connect properly to the database."})
-    }
+  try {
+    const users = await db.query(`SELECT * FROM users`);
+    res.json(users.rows);
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "Could not connect properly to the database." });
+  }
 });
 
 // This creates a new user
 app.post("/users", async (req, res) => {
-    try {
-        const newUser = await db.query(`INSERT INTO users (username) VALUES ($1) RETURNING *`,
-        [req.body.username]);
-        res.json(newUser.rows);
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({ error: "It was not possible to create a new user" });
-    }
+  try {
+    const newUser = await db.query(
+      `INSERT INTO users (username) VALUES ($1) RETURNING *`,
+      [req.body.username]
+    );
+    res.json(newUser.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: "It was not possible to create a new user" });
+  }
 });
 
 // This deletes an existing user
 app.delete("/users/:id", async (req, res) => {
-    const selectedId = parseInt(req.params.id);
+  const selectedId = parseInt(req.params.id);
 
-    try {
-        await db.query(`DELETE FROM users Where id=$1`,
-        [selectedId]);
-        res.json({ message: `User with id ${selectedId} deleted successfully!` });
-    } catch (err) {
-        console.log(err);
-        res.json({ error: "Unsuccessul action! The user was not found." })
-    }
+  try {
+    await db.query(`DELETE FROM users Where id=$1`, [selectedId]);
+    res.json({ message: `User with id ${selectedId} deleted successfully!` });
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "Unsuccessul action! The user was not found." });
+  }
 });
 
 // Books HTTP methods
 // List all the stored books
 app.get("/books", async (req, res) => {
-    try {
-        const books = await db.query(`
+  try {
+    const books = await db.query(`
         SELECT * FROM books;`);
-        res.json(books.rows);
-    } catch (err) {
-        console.log(err);
-        res.json({ error: "Couldn't find the requested data. User or book does not exist."})
-    }
+    res.json(books.rows);
+  } catch (err) {
+    console.log(err);
+    res.json({
+      error: "Couldn't find the requested data. User or book does not exist.",
+    });
+  }
 });
 
 // Adds a new book
 app.post("/books", async (req, res) => {
-    try {
-        const newBook = await db.query(`
+  try {
+    const newBook = await db.query(
+      `
         INSERT INTO books (title, author, cover)
         VALUES ($1, $2, $3) RETURNING *`,
-        [req.body.title, req.body.author, req.body.cover]);
-        res.json(newBook.rows);
-    } catch (err) {
-        console.log(err);
-        res.json({ error: "Could not create a new record on the database." });
-    }
+      [req.body.title, req.body.author, req.body.cover]
+    );
+    res.json(newBook.rows);
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "Could not create a new record on the database." });
+  }
 });
 
 // Review HTTP Methods
 // list all the book reviews from a certain user
 app.get("/reviews/:id", async (req, res) => {
-    const userId = parseInt(req.params.id);
+  const userId = parseInt(req.params.id);
 
-    try {
-        const reviews = await db.query(`
+  try {
+    const reviews = await db.query(
+      `
         SELECT title, author, cover, score, finish_date, text_content, book_id
         FROM reviews
         JOIN users ON users.id=user_id
         JOIN books ON books.id=book_id
         WHERE user_id=$1
-        ORDER BY book_id ASC`, [userId]);
-        res.json(reviews.rows);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Could not complete the action." });
-    }
+        ORDER BY book_id ASC`,
+      [userId]
+    );
+    res.json(reviews.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Could not complete the action." });
+  }
 });
 
 // Creates a new review for a book
 app.post("/reviews", async (req, res) => {
-    const userId = parseInt(req.body.user_id);
-    const bookId = parseInt(req.body.book_id);
+  const userId = parseInt(req.body.user_id);
+  const bookId = parseInt(req.body.book_id);
 
-    try {
-        const newReview = await db.query(`
+  try {
+    const newReview = await db.query(
+      `
         INSERT INTO reviews (score, finish_date, text_content, user_id, book_id)
-        VALUES ($1, $2, $3, $4, $5) RETURNING *`, [
+        VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+      [
         req.body.score,
         req.body.finish_date,
         req.body.text_content,
         userId,
-        bookId]);
-        res.json(newReview.rows);
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({ error: "Could not create the review, try again."})
-    }
+        bookId,
+      ]
+    );
+    res.json(newReview.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: "Could not create the review, try again." });
+  }
 });
 
 // Updates an existing review
 app.put("/reviews", async (req, res) => {
-    const bookId = parseInt(req.body.book_id);
-    const userId = parseInt(req.body.user_id);
+  const bookId = parseInt(req.body.book_id);
+  const userId = parseInt(req.body.user_id);
 
-    try {
-        const updatedReview = await db.query(`
+  try {
+    const updatedReview = await db.query(
+      `
         UPDATE reviews
         SET score=$1, finish_date=$2, text_content=$3
         WHERE user_id=$4 AND book_id=$5
-        RETURNING *`, [req.body.score, req.body.finish, req.body.opinion, userId, bookId]);
-        res.json(updatedReview.rows);
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({ error: "Could not update the review, try again." });
-    }
+        RETURNING *`,
+      [req.body.score, req.body.finish, req.body.opinion, userId, bookId]
+    );
+    res.json(updatedReview.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: "Could not update the review, try again." });
+  }
 });
 
 // Deletes an existing review
 app.delete("/reviews", async (req, res) => {
-    const bookId = parseInt(req.body.book);
-    const userId = parseInt(req.body.user);
+  const bookId = parseInt(req.body.book);
+  const userId = parseInt(req.body.user);
 
-    try {
-        await db.query(`DELETE FROM reviews WHERE book_id=$1 AND user_id=$2`,
-        [bookId, userId]);
-        res.json({ message: "Review deleted successfully!"});
-    } catch (err) {
-        res.status(400).json({ error: "Coult not delete the review, try again." });
-    }
+  try {
+    await db.query(`DELETE FROM reviews WHERE book_id=$1 AND user_id=$2`, [
+      bookId,
+      userId,
+    ]);
+    res.json({ message: "Review deleted successfully!" });
+  } catch (err) {
+    res.status(400).json({ error: "Coult not delete the review, try again." });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running successfully on port ${port}`);
+  console.log(`Server running successfully on port ${port}`);
 });
--- index.js
+++ index.js
@@ -7,16 +7,16 @@ import { password } from "./secret.js"; // You can delete this line
 // Initialize express application
 const app = express();
 const port = 3000;
-const LOCAL_URL = "http://localhost:4000"
+const LOCAL_URL = "http://localhost:4000";
 
 // Connect to the database
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "book_notes", // Here you can place the name you gave to the database
-    password: password, // Here you can place your postgres password
-    port: 5432,
-})
+  user: "postgres",
+  host: "localhost",
+  database: "book_notes", // Here you can place the name you gave to the database
+  password: password, // Here you can place your postgres password
+  port: 5432,
+});
 db.connect();
 
 // Middlewares
@@ -25,164 +25,168 @@ app.use(express.static("public"));
 app.use(express.json());
 
 async function checkBook(title) {
-    const books = await axios.get(`${LOCAL_URL}/books`);
-    const book = books.data.find((book) => book.title.toLowerCase() === title.toLowerCase());
-    return book;
+  const books = await axios.get(`${LOCAL_URL}/books`);
+  const book = books.data.find(
+    (book) => book.title.toLowerCase() === title.toLowerCase()
+  );
+  return book;
 }
 
 let currentUserId = 0; // 0 is default before entering the data
 
 app.get("/", async (req, res) => {
-    const users = await axios.get(`${LOCAL_URL}/users`);
-    res.render("index.ejs", { users: users.data });
+  const users = await axios.get(`${LOCAL_URL}/users`);
+  res.render("index.ejs", { users: users.data });
 });
 
 app.get("/home", async (req, res) => {
-    try {
-        const books = await axios.get(`${LOCAL_URL}/reviews/${currentUserId}`);
-        res.render("home.ejs", { books: books.data });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const books = await axios.get(`${LOCAL_URL}/reviews/${currentUserId}`);
+    res.render("home.ejs", { books: books.data });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/home", async (req, res) => {
-    const userId = parseInt(req.body.user);
+  const userId = parseInt(req.body.user);
 
-    try {
-        const books = await axios.get(`${LOCAL_URL}/reviews/${userId}`);
-        currentUserId = userId;
-        res.render("home.ejs", { books: books.data });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const books = await axios.get(`${LOCAL_URL}/reviews/${userId}`);
+    currentUserId = userId;
+    res.render("home.ejs", { books: books.data });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/new", async (req, res) => {
-    switch (req.body.new) {
-        case "user":
-            res.render("new.ejs", { user: "user" });
-            break;
+  switch (req.body.new) {
+    case "user":
+      res.render("new.ejs", { user: "user" });
+      break;
 
-        case "book":
-            res.render("new.ejs", { book: "book" });
-            break;
+    case "book":
+      res.render("new.ejs", { book: "book" });
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 });
 
 app.post("/createUser", async (req, res) => {
-    try {
-        await axios.post(`${LOCAL_URL}/users`, req.body);
-        res.redirect("/home");
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await axios.post(`${LOCAL_URL}/users`, req.body);
+    res.redirect("/home");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/createBook", async (req, res) => {
-    let bookId = 0;
+  let bookId = 0;
 
-    try {
-        // Check if the book already exists on the database
-        if (await checkBook(req.body.title)) {
-            const books = await axios.get(`${LOCAL_URL}/books`);
-            const book = books.data.find((book) => book.title.toLowerCase() === req.body.title.toLowerCase());
-            bookId = book.id;
+  try {
+    // Check if the book already exists on the database
+    if (await checkBook(req.body.title)) {
+      const books = await axios.get(`${LOCAL_URL}/books`);
+      const book = books.data.find(
+        (book) => book.title.toLowerCase() === req.body.title.toLowerCase()
+      );
+      bookId = book.id;
 
-            await axios.post(`${LOCAL_URL}/reviews`, {
-                book_id: bookId,
-                user_id: currentUserId,
-                score: req.body.score,
-                finish_date: req.body.finish,
-                text_content: req.body.opinion,
-            });
-            res.redirect("/home");
+      await axios.post(`${LOCAL_URL}/reviews`, {
+        book_id: bookId,
+        user_id: currentUserId,
+        score: req.body.score,
+        finish_date: req.body.finish,
+        text_content: req.body.opinion,
+      });
+      res.redirect("/home");
 
-        // If it doesn't exist, it creates a new record on the database.
-        } else {
-            const newBook = await axios.post(`${LOCAL_URL}/books`, {
-                title: req.body.title,
-                author: req.body.author,
-                cover: req.body.cover,
-            });
-            bookId = parseInt(newBook.data[0].id);
+      // If it doesn't exist, it creates a new record on the database.
+    } else {
+      const newBook = await axios.post(`${LOCAL_URL}/books`, {
+        title: req.body.title,
+        author: req.body.author,
+        cover: req.body.cover,
+      });
+      bookId = parseInt(newBook.data[0].id);
 
-            await axios.post(`${LOCAL_URL}/reviews`, {
-                book_id: bookId,
-                user_id: currentUserId,
-                score: req.body.score,
-                finish_date: req.body.finish,
-                text_content: req.body.opinion,
-            });
+      await axios.post(`${LOCAL_URL}/reviews`, {
+        book_id: bookId,
+        user_id: currentUserId,
+        score: req.body.score,
+        finish_date: req.body.finish,
+        text_content: req.body.opinion,
+      });
 
-            res.redirect("/home");
-        }
-    } catch (err) {
-        console.log(err);
+      res.redirect("/home");
     }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/options", async (req, res) => {
-    // Checks the option selected by the user (Edit or Delete)
-    if (req.body.edit) {
+  // Checks the option selected by the user (Edit or Delete)
+  if (req.body.edit) {
+    try {
+      const bookId = parseInt(req.body.edit);
 
-        try {
-        const bookId = parseInt(req.body.edit);
+      const reviews = await axios.get(`${LOCAL_URL}/reviews/${currentUserId}`);
+      const selectedReview = reviews.data.find(
+        (review) => review.book_id === bookId
+      );
 
-        const reviews = await axios.get(`${LOCAL_URL}/reviews/${currentUserId}`);
-        const selectedReview = reviews.data.find((review) => review.book_id === bookId);
-
-        res.render("options.ejs", {
-            edit: selectedReview,
-            userId: currentUserId,
-         });
-        } catch (err) {
-            console.log(err);
-        }
-    } else {
-        const delIds = {
-            book: parseInt(req.body.delete),
-            user: currentUserId,
-        };
-
-        res.render("options.ejs", { del: delIds });
+      res.render("options.ejs", {
+        edit: selectedReview,
+        userId: currentUserId,
+      });
+    } catch (err) {
+      console.log(err);
     }
+  } else {
+    const delIds = {
+      book: parseInt(req.body.delete),
+      user: currentUserId,
+    };
+
+    res.render("options.ejs", { del: delIds });
+  }
 });
 
 app.post("/edit", async (req, res) => {
-    try {
-        await axios.put(`${LOCAL_URL}/reviews`, req.body);
-        res.redirect("/home");
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await axios.put(`${LOCAL_URL}/reviews`, req.body);
+    res.redirect("/home");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/delete", async (req, res) => {
-    try {
-        // Checks if the user really wants to delete the review
-        switch (req.body.option) {
-            case "del":
-                await axios.delete(`${LOCAL_URL}/reviews`, req.body);
-                res.redirect("/home");
-                break;
+  try {
+    // Checks if the user really wants to delete the review
+    switch (req.body.option) {
+      case "del":
+        await axios.delete(`${LOCAL_URL}/reviews`, req.body);
+        res.redirect("/home");
+        break;
 
-            case "cancel":
-                res.redirect("/home");
-                break;
+      case "cancel":
+        res.redirect("/home");
+        break;
 
-            default:
-                break;
-        }
-
-    } catch (err) {
-        console.log(err);
+      default:
+        break;
     }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
+  console.log(`Server running on port ${port}.`);
 });
